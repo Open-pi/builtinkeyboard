b4f6ada6a9e4d1ab2561f3604e6bcc1085057420
maistrox9 <youtoukabri@protonmail.com>
2022-04-01 23:49:44 +0200

    Add the German language with QWERTY and QWERTZ layouts

--- lib/language.dart
+++ lib/language.dart
@@ -1,6 +1,7 @@
 enum Language {
   EN,
   FR,
+  DE,
 }
 enum Layout {
   QWERTY,
@@ -11,6 +12,7 @@ enum Layout {
 var languageConfig = {
   Language.EN: englishConfig,
   Language.FR: frenchConfig,
+  Language.DE: germanConfig,
 };
 
 // Languages Configurations
@@ -42,5 +44,20 @@ var frenchConfig = {
     'horizontalSpacing': '6.0',
     'topLength': '10',
     'middleLength': '9',
+  },
+};
+
+var germanConfig = {
+  Layout.QWERTY: <String, String>{
+    'layout': 'qwertyuiopüasdfghjklöäzxcvbnmß',
+    'horizontalSpacing': '2.5',
+    'topLength': '11',
+    'middleLength': '11',
+  },
+  Layout.QWERTZ: <String, String>{
+    'layout': 'qwertzuiopüasdfghjklöäyxcvbnmß',
+    'horizontalSpacing': '2.5',
+    'topLength': '10',
+    'middleLength': '11',
   },
 };
